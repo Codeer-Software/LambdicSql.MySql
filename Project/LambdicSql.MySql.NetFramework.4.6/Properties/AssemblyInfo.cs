3596e16bff3cf096169cfa47de67c36e3a3de423
Tatsuya Ishikawa <stone.guitar.777@gmail.com>
2017-07-15 13:43:41 +0900

    version up.

---
+++ Project/LambdicSql.MySql.NetFramework.4.6/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LambdicSql.MySql.NetFramework.4.6")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LambdicSql.MySql.NetFramework.4.6")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cb03134b-6532-42ed-9848-d05f5b8d3e25")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
